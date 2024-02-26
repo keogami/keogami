aa4f19afe10fbfd34493223afb4de036b9435ac9
keogami <41939011+keogami@users.noreply.github.com>
2024-02-26 15:03:17 +0300

    feat: add tailwind

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {
+			colors: {
+				'white': '#fff',
+				'black': '#070507',
+				'lavender': '#B195C7',
+				'dark-lavender': '#4d385c',
+			}
+		},
+	},
+	plugins: [],
+}
